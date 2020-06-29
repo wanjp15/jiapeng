f6d7ee971e9a675036a31e45b8b45fdee1efe51c
万嘉鹏 <wanjp@MacBook-Pro.local>
2020-06-28 22:19:21 -0700

    Updates

---
+++ precache-manifest.046aba1608afa2bfa547ab62c8aea111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665b9a9a6011042fd2922593b0aee684",
+    "url": "/jiapeng/index.html"
+  },
+  {
+    "revision": "aaee972d10a65419c56b",
+    "url": "/jiapeng/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d266738e1efe8acc168",
+    "url": "/jiapeng/static/js/2.f450c5a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jiapeng/static/js/2.f450c5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaee972d10a65419c56b",
+    "url": "/jiapeng/static/js/main.f123528a.chunk.js"
+  },
+  {
+    "revision": "516eb32923d77e9a6e7a",
+    "url": "/jiapeng/static/js/runtime-main.56198bd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jiapeng/static/media/logo.5d5d9eef.svg"
+  }
+]);
